e99946333e5674a2bcb544d71eb5279c8f102de5
eunjineee <yej6642@naver.com>
2022-12-18 17:01:36 +0900

    1218 검색창

--- final-pjt-back/movies/urls.py
+++ final-pjt-back/movies/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'movies'
 
 urlpatterns = [
+    path('search/<str:search>', views.movie_search),
     path('', views.movie_list),
     path('<int:movie_pk>/review/', views.review_list_create),
     path('<int:movie_pk>/review/<int:review_pk>/', views.review_detail),
--- final-pjt-front/src/views/HomeView.vue
+++ final-pjt-front/src/views/HomeView.vue
@@ -69,15 +69,10 @@ export default {
     getmoviedata() {
       const movie_length_latest = this.$store.movie_latest
       if (movie_length_latest === undefined) {
-      const movie_length = this.$store.movies
-      console.log(movie_length)
-      axios.get(`${API_URL}/movies`)
-        .then((res) => {
-          this.$store.dispatch('getMovieData', res.data)
-        })
-        .catch((err) => { 
-          console.log(err)
-        })}
+        axios.get(`${API_URL}/movies`)
+          .then((res) => {
+            this.$store.dispatch('getMovieData', res.data)
+          })}
     },
     getmovielatest() {
       const movie_length = this.$store.movie_latest
